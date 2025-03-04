5d7c670f9265635eea685439e628fdcee9d5dd56
6reenhorn <coding.cyrilj777@gmail.com>
2025-03-04 21:54:14 +0800

    Backend url modified

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to MovieHaven API</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', home),
 ]
