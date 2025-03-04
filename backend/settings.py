2d2103de2c89a92bfb77acb5b1cf3d670a90d8d0
6reenhorn <coding.cyrilj777@gmail.com>
2025-03-04 20:39:24 +0800

    Modified settings

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-18nz=@1sfq_esot-!fpajxs5a@fk1!!d54_(p4tx1-7kgm$g^2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.252.243.166", "*"]
 
 
 # Application definition
